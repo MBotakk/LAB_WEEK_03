2c171c385b51222334f5d41eef94632ede051dc8
MBotakk <samuel.itachis4@gmail.com>
2025-09-15 20:34:40 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_03/DetailFragment.kt
@@ -1,6 +1,5 @@
 package com.example.lab_week_03
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,22 +8,17 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class DetailFragment : Fragment() {
-    companion object {
-        private const val COFFEE_ID = "COFFEE_ID"
-        fun newInstance(coffeeId: Int) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(COFFEE_ID, coffeeId)
-                }
-            }
-    }
 
     private val coffeeTitle: TextView?
         get() = view?.findViewById(R.id.coffee_title)
+
     private val coffeeDesc: TextView?
         get() = view?.findViewById(R.id.coffee_desc)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
@@ -34,9 +28,8 @@ class DetailFragment : Fragment() {
         setCoffeeData(coffeeId)
     }
 
-    @SuppressLint("SetTextI18n")
-    fun setCoffeeData(id: Int) {
-        when(id){
+    private fun setCoffeeData(id: Int) {
+        when (id) {
             R.id.affogato -> {
                 coffeeTitle?.text = getString(R.string.affogato_title)
                 coffeeDesc?.text = getString(R.string.affogato_desc)
@@ -49,14 +42,10 @@ class DetailFragment : Fragment() {
                 coffeeTitle?.text = getString(R.string.latte_title)
                 coffeeDesc?.text = getString(R.string.latte_desc)
             }
-            R.id.macchiato -> {
-                coffeeTitle?.text = "MACCHIATO"
-                coffeeDesc?.text = "A shot of espresso with a small amount of milk."
-            }
-            R.id.cappuccino -> {
-                coffeeTitle?.text = "CAPPUCCINO"
-                coffeeDesc?.text = "Espresso with steamed milk and milk foam."
-            }
         }
+    }
+
+    companion object {
+        const val COFFEE_ID = "COFFEE_ID"
     }
 }
--- app/src/main/java/com/example/lab_week_03/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -1,56 +1,40 @@
 package com.example.lab_week_03
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
-// Define the interface for communication
-interface CoffeeListener {
-    fun onCoffeeSelected(id: Int)
-}
-
-class ListFragment : Fragment(), View.OnClickListener {
-
-    private lateinit var coffeeListener: CoffeeListener
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is CoffeeListener) {
-            coffeeListener = context
-        } else {
-            throw RuntimeException("$context must implement CoffeeListener")
-        }
-    }
+class ListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val coffeeList = listOf<View>(
             view.findViewById(R.id.affogato),
             view.findViewById(R.id.americano),
-            view.findViewById(R.id.latte),
-            view.findViewById(R.id.macchiato),
-            view.findViewById(R.id.cappuccino)
+            view.findViewById(R.id.latte)
         )
 
-        coffeeList.forEach {
-            it.setOnClickListener(this)
-        }
-    }
+        coffeeList.forEach { coffee ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(DetailFragment.COFFEE_ID, coffee.id)
 
-    override fun onClick(v: View?) {
-        v?.let { coffee ->
-            coffeeListener.onCoffeeSelected(coffee.id)
+            coffee.setOnClickListener(
+                Navigation.createNavigateOnClickListener(
+                    R.id.action_listFragment_to_detailFragment,
+                    fragmentBundle
+                )
+            )
         }
     }
 }
